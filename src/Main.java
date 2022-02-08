4aac7239d04070c84957a17c4833bf851a233a69
dilanAKYEL <dln.1087.yldrm@outlook.com>
2022-02-08 19:46:06 +0300

    sınıf geçme programı tamamlandı

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String usarName, password;
+        int mat, turkce, kimya, fizik, müzik;
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Kullanıcı adını giriniz:");
-        usarName = input.nextLine();
 
-        System.out.print("Şifre giriniz:");
-        password = input.nextLine();
-        if (usarName.equals("patika") && password.equals("dlan")) {
-            System.out.print("Başarıyla Giriş Yaptınız !!!");
-        } else {
-            System.out.print("Şifreniz Yanlış, Sıfırlamak için R'ye Çıkmak için Q'ya basın ! : ");
+        System.out.print("Matematik notunuzu giriniz:");
+        mat=input.nextInt();
 
-            String sifirlama = input.nextLine();
-            if (sifirlama.equals("R") || sifirlama.equals("r")) {
-                System.out.print("Yeni Şifrenizi Giriniz : ");
-                password = input.nextLine();
-                if (password.equals("dlandpa")) {
-                    System.out.print("Şifre oluşturulamadı, lütfen başka şifre giriniz.");
-                } else {
-                    System.out.print("Şifre Oluşturuldu");
-                }
-            } else if (sifirlama.equals("Q") || sifirlama.equals("q")) {
-                System.out.print("Çıkış Yapılıyor...");
-            }
+        System.out.print("Fizik notunuzu giriniz:");
+        fizik=input.nextInt();
+
+        System.out.print("Türkçe notunuzu giriniz:");
+        turkce=input.nextInt();
+
+        System.out.print("Kimya notunuzu giriniz:");
+        kimya=input.nextInt();
+
+        System.out.print("Müzik notunuzu giriniz:");
+        müzik=input.nextInt();
+
+        double avarage=(mat+fizik+turkce+kimya+müzik)/5;
+        if (avarage<=55){
+            System.out.print("Sınıfta Kaldınız!");
         }
+        else {
+            System.out.print("Tebrikler , sınıfı geçtiniz!");
+        }
+        System.out.print("Ortalamanız:"+avarage);
     }
 }
 
